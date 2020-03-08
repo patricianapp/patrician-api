f29b57f0bf3f25dc5e5aa84c1a3549107aa629b5
Elias Jackson <ejthedj0@gmail.com>
2020-03-08 09:50:04 -0700

    Database setup

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+
+import { ConnectionOptions } from 'typeorm/connection/ConnectionOptions';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const typeOrmConfig: ConnectionOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOSTNAME || 'localhost',
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+  username: process.env.POSTGRES_USERNAME || 'elias',
+  password: process.env.POSTGRES_PASSWORD || 'password',
+  database: process.env.POSTGRES_DB_NAME || 'patrician',
+  entities: [__dirname + '/entities/*.ts'], // '/../**/*.entity.{js,ts}'
+  synchronize: (process.env.TYPEORM_SYNC === 'true') || true,
+  logger: 'advanced-console',
+  logging: 'all',
+  cache: true,
+  dropSchema: true,
+};
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { Container } from 'typedi';
 import * as TypeORM from 'typeorm';
 import * as TypeGraphQL from 'type-graphql';
 
+import { typeOrmConfig } from './config';
+
 import { RecipeResolver } from './resolvers/recipe-resolver';
 import { RateResolver } from './resolvers/rate-resolver';
 import { Recipe } from './entities/recipe';
@@ -20,20 +22,7 @@ TypeORM.useContainer(Container);
 async function bootstrap() {
   try {
     // create TypeORM connection
-    await TypeORM.createConnection({
-      type: 'mysql',
-      database: 'type-graphql',
-      username: 'root', // fill this with your username
-      password: 'qwerty123', // and password
-      port: 3306,
-      host: 'localhost',
-      entities: [Recipe, Rate, User],
-      synchronize: true,
-      logger: 'advanced-console',
-      logging: 'all',
-      dropSchema: true,
-      cache: true,
-    });
+    await TypeORM.createConnection(typeOrmConfig);
 
     // build TypeGraphQL executable schema
     const schema = await TypeGraphQL.buildSchema({
