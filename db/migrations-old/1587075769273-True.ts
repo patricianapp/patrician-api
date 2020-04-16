ec3e93f8ec8f49f3fc972df53c41d20648124f7f
Elias Jackson <ejthedj0@gmail.com>
2020-04-16 16:00:42 -0700

    Recreated database again, added signUp()

--- db/migrations-old/1587075769273-True.ts
+++ db/migrations-old/1587075769273-True.ts
@@ -5,8 +5,6 @@ export class True1587075769273 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "reviews" ("id" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "created_by_id" character varying NOT NULL, "updated_at" TIMESTAMP DEFAULT now(), "updated_by_id" character varying, "deleted_at" TIMESTAMP, "deleted_by_id" character varying, "version" integer NOT NULL, "collection_item_id" character varying NOT NULL, "rating" character varying, "title" character varying, "review_text" character varying, CONSTRAINT "PK_231ae565c273ee700b283f15c1d" PRIMARY KEY ("id"))`, undefined);
-        await queryRunner.query(`ALTER TABLE "items" DROP COLUMN "rating"`, undefined);
-        await queryRunner.query(`ALTER TABLE "items" DROP COLUMN "review"`, undefined);
         await queryRunner.query(`ALTER TABLE "users" ADD "password" character varying NOT NULL`, undefined);
         await queryRunner.query(`ALTER TABLE "users" ADD "salt" character varying NOT NULL`, undefined);
         await queryRunner.query(`ALTER TABLE "users" ADD "account_settings" jsonb`, undefined);
@@ -22,8 +20,6 @@ export class True1587075769273 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "account_settings"`, undefined);
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "salt"`, undefined);
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "password"`, undefined);
-        await queryRunner.query(`ALTER TABLE "items" ADD "review" character varying`, undefined);
-        await queryRunner.query(`ALTER TABLE "items" ADD "rating" character varying`, undefined);
         await queryRunner.query(`DROP TABLE "reviews"`, undefined);
     }
 
